3de02c581b72949ec4906a675764c27db41b92cd
Rifat Arefin <rifatarefin19@gmail.com>
2016-04-19 19:38:59 +0600

    memoization corrected1

--- coinchange_memoization.cpp
+++ coinchange_memoization.cpp
@@ -1,39 +1,45 @@
-
 #define f(a,i,n) for(int i=a;i<n;i++)
 #include<bits/stdc++.h>
 using namespace std;
-int sol[10][1000];
+int sol[100][1000];
+int W,N;
 int rrr(int *a, int *b,int n,int w)
 {
     if(w==0)return 0;
     if(w<0 || n==0)return 1000;
+
+    if(sol[n][w]!=-1)return sol[n][w];
+
     if(a[n]>w || b[n]<=0)
     {
-        if(sol[n-1][w]==-1)sol[n-1][w]=rrr(a,b,n-1,w);
-        return sol[n-1][w];
+        return sol[n-1][w]=rrr(a,b,n-1,w);
+
     }
 
+    //sol[n-1][w]=
     int c=rrr(a,b,n-1,w);
     b[n]--;
 
+    //sol[n][w-a[n]]=
     int e=rrr(a,b,n,w-a[n])+1;
     b[n]++;
-    return sol[n][w]=min(c,e);
+    int d=min(c,e);
+    if(d==e)cout<<a[n]<<ends;
+    if(w==W && n==N)cout<<endl;
+    return sol[n][w]=d;
 
 }
 
 int main()
 {
-    //ifstream fin;
-    //fin.open("new.txt");
-    int n,w;
-    cin>>n;
-    int a[n+1],b[n+1];
-    f(1,i,n+1)cin>>a[i]>>b[i];
-    cin>>w;
+
+    cin>>N;
+    int a[N+1],b[N+1];
+    f(1,i,N+1)cin>>a[i]>>b[i];
+    cin>>W;
 
     memset(sol,-1,sizeof sol);
-    cout<<rrr(a,b,n,w);
+    cout<<rrr(a,b,N,W);
 
 
 }

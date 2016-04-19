3112ade28457f2b51ef6d062860181c1e792b4ba
Rifat Arefin <rifatarefin19@gmail.com>
2016-04-19 18:55:54 +0600

    memoization

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,17 +1,24 @@
+
 #define f(a,i,n) for(int i=a;i<n;i++)
 #include<bits/stdc++.h>
 using namespace std;
+int sol[10][200];
 int rrr(int *a, int *b,int n,int w)
 {
     if(w==0)return 0;
-    if(w<0)return 1000;
-    if(n==0)return 1000;
-    if(a[n]>w || b[n]<=0)return rrr(a,b,n-1,w);
+    if(w<0 || n==0)return 1000;
+    if(a[n]>w || b[n]<=0)
+    {
+        if(sol[n-1][w]==-1)sol[n-1][w]=rrr(a,b,n-1,w);
+        return sol[n-1][w];
+    }
 
-    int c=rrr(a,b,n-1,w);
+    if(sol[n-1][w]==-1)sol[n-1][w]=rrr(a,b,n-1,w);
+    int c=sol[n-1][w];
     b[n]--;
 
-    int e=rrr(a,b,n,w-a[n])+1;
+    if(sol[n][w-a[n]]==-1)sol[n][w-a[n]]=rrr(a,b,n,w-a[n])+1;
+    int e=sol[n][w-a[n]];
     b[n]++;
     return min(c,e);
 
@@ -26,6 +33,8 @@ int main()
     int a[n+1],b[n+1];
     f(1,i,n+1)fin>>a[i]>>b[i];
     fin>>w;
+
+    memset(sol,-1,sizeof sol);
     cout<<rrr(a,b,n,w);
 
 

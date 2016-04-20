fed7473612b8e8901537aa03253948ba38bde3bf
Rifat Arefin <rifatarefin19@gmail.com>
2016-04-20 10:00:58 +0600

    coinchange corrected2

--- coinchange_memoization.cpp
+++ coinchange_memoization.cpp
@@ -1,34 +1,43 @@
 #define f(a,i,n) for(int i=a;i<n;i++)
 #include<bits/stdc++.h>
 using namespace std;
-int sol[100][1000];
+int sol[100][1000][100];
 int W,N;
-int rrr(int *a, int *b,int n,int w)
+int rrr(int *a, int *b,int n,int w,int t)
 {
-    if(w==0)return 0;
+    if(w==0)return  0;
     if(w<0 || n==0)return 1000;
 
-    if(sol[n][w]!=-1)return sol[n][w];
-
-    if(a[n]>w || b[n]<=0)
+    if(sol[n][w][t]!=-1)
     {
-        return sol[n-1][w]=rrr(a,b,n-1,w);
 
+        return sol[n][w][t];
+    }
+
+    if(a[n]>w || t<=0)
+    {
+        sol[n][w][t]=rrr(a,b,n-1,w,b[n-1]);
+        return sol[n][w][t];
     }
 
     //sol[n-1][w]=
-    int c=rrr(a,b,n-1,w);
+    int c=rrr(a,b,n-1,w,b[n-1]);
     b[n]--;
 
-    //sol[n][w-a[n]]=
-    int e=rrr(a,b,n,w-a[n])+1;
+    int e=rrr(a,b,n,w-a[n],b[n])+1;
     b[n]++;
+
     int d=min(c,e);
-    if(d==e)cout<<a[n]<<ends;
-    if(w==W && n==N)cout<<endl;
-    return sol[n][w]=d;
+    sol[n][w][t]=d;
+
+    return sol[n][w][t];
 
 }
+
+
+
+
+
 
 int main()
 {
@@ -37,9 +46,40 @@ int main()
     int a[N+1],b[N+1];
     f(1,i,N+1)cin>>a[i]>>b[i];
     cin>>W;
-
     memset(sol,-1,sizeof sol);
-    cout<<rrr(a,b,N,W);
-
+    int ret  = rrr(a,b,N,W,b[N]);
+    cout << ret << endl;
+//    f(0,i,N+1)
+//    {
+//        f(0,j,W+1)cout<<sol[i][j]<<ends;
+//        cout<<endl;
+//    }
+    //cout<<sol[N][W];
 
 }
+
+
+
+
+/*
+
+
+2
+1 10
+2 3
+14
+
+
+2
+1 10
+20 1
+9
+
+
+
+
+
+
+
+
+  */
